c57064388e8cc9ad861b1b5c803679dee4eb1ad4
hamjiwon <skxo0039@gmail.com>
2025-01-02 19:58:06 +0900

    Add: 정수 내림차순으로 배치하기

---
+++ src/level1/Q_12933.java
@@ -0,0 +1,15 @@
+package level1;
+
+import java.util.*;
+
+public class Q_12933 {
+    public long solution(long n) {
+        String[] list = String.valueOf(n).split("");
+        Arrays.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for (String aList : list) sb.append(aList);
+
+        return Long.parseLong(sb.reverse().toString());
+    }
+}
