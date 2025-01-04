68f745e9a0c3414d78190f7bc81d2398de9155c9
hamjiwon <skxo0039@gmail.com>
2025-01-04 17:42:17 +0900

    Add: 가운데 글자 가져오기

---
+++ src/level1/Q_12903.java
@@ -0,0 +1,9 @@
+package level1;
+
+import java.util.Arrays;
+
+public class Q_12903 {
+    public String solution(String s) {
+        return s.substring((s.length()-1) / 2, s.length() / 2 + 1);
+    }
+}
