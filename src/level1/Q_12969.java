2d78b59c3ffc6313920e95813f718aa7c968b028
hamjiwon <skxo0039@gmail.com>
2025-01-05 19:07:38 +0900

    Add: 직사각형 별찍기

---
+++ src/level1/Q_12969.java
@@ -0,0 +1,20 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Q_12969 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
