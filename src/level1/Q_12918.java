516b70b4c106a5184c2a3355a74d57c69a6acbec
hamjiwon <skxo0039@gmail.com>
2025-01-05 17:24:27 +0900

    Add: 문자열 다루기 기본

---
+++ src/level1/Q_12918.java
@@ -0,0 +1,15 @@
+package level1;
+
+public class Q_12918 {
+    public boolean solution(String s) {
+        if (s.length() == 4 || s.length() == 6) {
+
+            try {
+                int x = Integer.parseInt(s);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        } else return false;
+    }
+}
