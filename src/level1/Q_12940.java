e8258a3b5bda9d2cb72da3cb4ce02add20fa8d7b
hamjiwon <skxo0039@gmail.com>
2025-01-10 15:59:02 +0900

    Add: 최대공약수와 최소공배수

---
+++ src/level1/Q_12940.java
@@ -0,0 +1,17 @@
+package level1;
+
+public class Q_12940 {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int max = 0;
+
+        for (int i = 1; i <= n && i <= m; i++) {
+            if (n % i == 0 && m % i == 0) max = i;
+        }
+
+        answer[0] = max;
+        answer[1] = n * m / max;
+
+        return answer;
+    }
+}
