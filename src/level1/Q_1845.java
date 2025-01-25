17376b4fc2c02bf556db51c0492442f61c2b269b
hamjiwon <skxo0039@gmail.com>
2025-01-25 14:32:19 +0900

    Add: 폰켓몬

---
+++ src/level1/Q_1845.java
@@ -0,0 +1,16 @@
+package level1;
+
+import java.util.*;
+
+public class Q_1845 {
+    public int solution(int[] nums) {
+        int answer = 0;
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) set.add(num);
+
+        answer = Math.min(set.size(), (nums.length / 2));
+
+        return answer;
+    }
+}
